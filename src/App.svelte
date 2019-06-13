edd466c3e9dd3c692a03270725539d0893b0f167
imhtop <nponmuthuselvam@gmail.com>
2019-06-13 21:03:54 +0530

    button control added

--- src/App.svelte
+++ src/App.svelte
@@ -1,28 +1,54 @@
 <script>
 	import {onMount} from 'svelte';
 
-	export let name = "Hello world";
 	let data = [];
+	let index = 0;
 	onMount(async function () {
 		const res = await fetch('https://hacker-news.firebaseio.com/v0/topstories.json');
 		const jsonIdData = await res.json();
-		jsonIdData.slice(0, 20).forEach(async (id) => {
+		jsonIdData.slice(0, 19).forEach(async (id) => {
 			const res = await fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)
 			const jsonNews = await res.json();
 			data = [...data, jsonNews]; 
 		});
 	});
 
+	function handleClick(event) {
+		if(event.keyCode === 39) {
+			if(index < data.length) {
+				index++;
+			}
+		}else if (event.keyCode === 37) {
+			if(index > 0) {
+				index--;
+			}
+		}
+	}
 </script>
 
 <style>
+	h1, h2 {
+		text-align: center;
+	}
 	a {
 		display: block;
 		margin: 10px;
 	}
-</style>
-<h1>headfeed</h1>
-{#each data as news, i}
-<a href="{news.url}">{i + 1}. &nbsp;{news.title}</a>
-{/each}
+	.btn-wrapper {
+		display: flex;
+		justify-content: center;
+	}
 
+</style>
+<nav><h1>headfeed</h1></nav>
+<main>
+{#if data.length}
+<h2><a href="{data[index].url}" >{index + 1}. &nbsp;{data[index].title}</a></h2>
+<div class="btn-wrapper">
+<button>Backward</button><button>Forward</button>
+</div>
+{:else} 
+<h2>loading..</h2>
+{/if}
+
+</main>
