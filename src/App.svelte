d9afce27a1ff6f6a61843baa27f9e21a5be3e997
imhtop <nponmuthuselvam@gmail.com>
2019-06-13 18:31:27 +0530

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,28 @@
+<script>
+	import {onMount} from 'svelte';
+
+	export let name = "Hello world";
+	let data = [];
+	onMount(async function () {
+		const res = await fetch('https://hacker-news.firebaseio.com/v0/topstories.json');
+		const jsonIdData = await res.json();
+		jsonIdData.slice(0, 20).forEach(async (id) => {
+			const res = await fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)
+			const jsonNews = await res.json();
+			data = [...data, jsonNews]; 
+		});
+	});
+
+</script>
+
+<style>
+	a {
+		display: block;
+		margin: 10px;
+	}
+</style>
+<h1>Top 20 headlines</h1>
+{#each data as news, i}
+<a href="{news.url}">{i + 1}. &nbsp;{news.title}</a>
+{/each}
+
