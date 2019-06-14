d2304142d9f366f764644bed4d2c35f7d608f073
imhtop <nponmuthuselvam@gmail.com>
2019-06-14 16:29:28 +0530

    news api added and little change

--- src/App.svelte
+++ src/App.svelte
@@ -1,63 +1,136 @@
 <script>
-	import {onMount} from 'svelte';
+  import { onMount } from "svelte";
 
-	let data = [];
-	let index = 0;
-	onMount(async function () {
-		const res = await fetch('https://hacker-news.firebaseio.com/v0/topstories.json');
-		const jsonIdData = await res.json();
-		jsonIdData.slice(0, 19).forEach(async (id) => {
-			const res = await fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)
-			const jsonNews = await res.json();
-			data = [...data, jsonNews]; 
-		});
-	});
-	function forward() {
-		if(index < data.length) {
-			index++;
-		}
-	}
-	function backward() {
-		if(index > 0) {
-			index--;
-		}
-	}
-	function handleKey(event) {
-		if(event.keyCode === 39) {
-			if(index < data.length) {
-				index++;
-			}
-		}else if (event.keyCode === 37) {
-			if(index > 0) {
-				index--;
-			}
-		}
-	}
+  let data = [];
+  let index = 0;
+ 
+  onMount(async function() {
+    getTopFromNewsAPI();
+  });
+async function getTopFromNewsAPI() {
+    const api = 'f4e25419df6f4a1aac2faaaf3ae149ff';
+    const res = await fetch(`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${api}`)
+    const jsonData = await res.json();
+    data = jsonData.articles;
+}
+async function getHackerNews() {
+    const res = await fetch(
+      "https://hacker-news.firebaseio.com/v0/topstories.json"
+    );
+    const jsonIdData = await res.json();
+    jsonIdData.slice(0, 21).forEach(async id => {
+      const res = await fetch(
+        `https://hacker-news.firebaseio.com/v0/item/${id}.json`
+      );
+      const jsonNews = await res.json();
+      data = [...data, jsonNews];
+    });
+  }
+  function forward() {
+    if (index < data.length-1) {
+      index++;
+      speak(data[index].title)
+    }
+  }
+  function backward() {
+    if (index > 0) {
+      index--;
+    speak(data[index].title)
+    }
+  }
+  function handleKey(event) {
+    if (event.keyCode === 39) {
+      if (index < data.length) {
+        index++;
+      }
+    } else if (event.keyCode === 37) {
+      if (index > 0) {
+        index--;
+      }
+    }
+  }
+  function speak(str) {
+      let msg = new SpeechSynthesisUtterance(str);
+      window.speechSynthesis.speak(msg);
+  }
 </script>
 
 <style>
-	h1, h2 {
-		text-align: center;
-	}
-	a {
-		display: block;
-		margin: 10px;
-	}
-	.btn-wrapper {
-		display: flex;
-		justify-content: center;
-	}
+   .material-icons{font-size: 40px;}
+  h1{
+    text-align: center;
+  }
+  nav h1{
+    font-size: 40px;
+  }
+  h1 span{
+    color: #FF9900;
+  }
+  a {
+    display: block;
+    margin: 10px;
+  }
 
+  button{
+    width: 60px;
+    height: 60px;
+    border-radius: 60px;
+    background-color: #FF9900;
+    color: white;
+   }
+   main{
+     display: flex;
+     height: 75%;
+     justify-content: center;
+     }
+   .card{
+     display: flex;
+     height: 100%;
+     width: 500px; 
+     /* border-radius: 15px; */
+     align-items: center;
+     background-color: bisque;
+     position: relative;
+   }
+  .backward-btn{
+    position: absolute;
+    left: 25%;
+    top: 75%;
+  }
+  .forward-btn{
+    position: absolute;
+    right: 25%;
+    top: 75%;
+  }
 </style>
-<nav><h1>headfeed</h1></nav>
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
+
+<nav>
+  <h1>head<span>feed</span></h1>
+
+</nav>
+
 <main>
-{#if data.length}
-<h2><a href="{data[index].url}" >{index + 1}. &nbsp;{data[index].title}</a></h2>
-<div class="btn-wrapper">
-<button on:click={backward}>Backward</button><button on:click={forward}>Forward</button>
-</div>
-{:else} 
-<h2>loading..</h2>
-{/if}
+  {#if data.length}
+   <img src="{data[index].urlToImage}" alt="{data[index].title}">
+  <div class="card">
+
+    <h1>
+      <a href={data[index].url}>{data[index].title}</a>
+    </h1>
+      <button class="backward-btn" on:click={backward}>
+            <i class="material-icons">keyboard_arrow_left</i>
+      </button>
+      <button class="forward-btn" on:click={forward}>
+            <i class="material-icons">keyboard_arrow_right</i>
+      </button>
+    </div>
+
+
+
+  {:else}
+    <h2>loading..</h2>
+  {/if}
 
 </main>
